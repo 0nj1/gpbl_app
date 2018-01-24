9089383565bc698654dc1b99d0862b07ed44bfc3
inoutch <akiandaki@gmail.com>
2018-01-24 18:31:54 +0900

    Add activities and projects

---
+++ src/main/java/com/onji/entity/Project.java
@@ -0,0 +1,53 @@
+package com.onji.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotNull
+    private String projectId;
+
+    @NotNull
+    private String projectKey;
+
+    @NotNull
+    private Integer sourceId;
+
+    @Length(max = 256)
+    private String name;
+
+    // TODO: Add department
+
+    private Timestamp actualStartDate;
+
+    private Timestamp actualEndDate;
+
+    @NotNull
+    private Integer languageId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @NotNull
+    private Timestamp createdAt;
+
+    @NotNull
+    private Timestamp updatedAt;
+
+    @NotNull
+    private Timestamp deletedAt;
+}
